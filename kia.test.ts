26846acee4ead99938cd6dcd6a94809e10928930
Harry Peach <harrypeach1999@gmail.com>
2020-05-19 15:39:02 +0100

    Added another test

--- kia.test.ts
+++ kia.test.ts
@@ -1,5 +1,8 @@
 import Kia from "./mod.ts";
-import { assertEquals } from "https://deno.land/std@0.51.0/testing/asserts.ts";
+import {
+	assertEquals,
+	assertThrowsAsync,
+} from "https://deno.land/std@0.51.0/testing/asserts.ts";
 
 Deno.test("Spinner isSpinning when running", async () => {
 	const kia = new Kia("");
@@ -15,3 +18,14 @@ Deno.test("Spinner !isSpinning when not running", async () => {
 		assertEquals(kia.isSpinning(), false);
 	});
 });
+
+Deno.test(
+	"Check renderNextFrame can't be called if spinner is running",
+	async () => {
+		const kia = await new Kia("").start();
+		await assertThrowsAsync(async () => {
+			await kia?.renderNextFrame();
+		}, Error);
+		await kia?.stop();
+	}
+);
