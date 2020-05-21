5caa47414380c49c09555a19e3630f69a372e141
Harry Peach <harrypeach1999@gmail.com>
2020-05-21 21:50:33 +0100

    Added tests for some of the main kia class

--- kia.test.ts
+++ kia.test.ts
@@ -1,26 +1,89 @@
 import Kia from "./mod.ts";
-import {
-	assertEquals,
-	assertThrowsAsync,
-} from "https://deno.land/std@0.51.0/testing/asserts.ts";
+import { assertThrowsAsync } from "https://deno.land/std@0.51.0/testing/asserts.ts";
+import { expect } from "https://deno.land/x/expect/mod.ts";
 
-Deno.test("Spinner isSpinning when running", async () => {
+async function setupTestFile(): Promise<[Deno.File, string]> {
+	const fileLocation = await Deno.makeTempFile({
+		prefix: "kia.test.",
+	});
+	return [
+		await Deno.open(fileLocation, {
+			read: true,
+			write: true,
+			create: true,
+		}),
+		fileLocation,
+	];
+}
+
+async function cleanupTestFile(file: Deno.File, fileName: string) {
+	file.close();
+	Deno.remove(fileName);
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+Deno.test("spinner isSpinning when running", async () => {
 	const kia = new Kia("");
 	await kia.start().then(() => {
-		assertEquals(kia.isSpinning(), true);
+		expect(kia.isSpinning()).toEqual(true);
 	});
 	await kia.stop();
 });
 
-Deno.test("Spinner !isSpinning when not running", async () => {
+Deno.test("spinner !isSpinning when not running", async () => {
 	const kia = await new Kia("").start();
 	await kia?.stop().then(() => {
-		assertEquals(kia.isSpinning(), false);
+		expect(kia.isSpinning()).toEqual(false);
 	});
 });
 
+Deno.test("stopAndPersist stops the spinner output", async () => {
+	const [testFile, testFileName] = await setupTestFile();
+
+	const kia = await new Kia({
+		text: "",
+		resource: testFile.rid,
+	}).start();
+	await kia?.stopAndPersist();
+
+	const sizeAfterStop = (await Deno.stat(testFileName)).size;
+	await sleep(1000);
+	expect(kia?.isSpinning()).toEqual(false);
+	expect(sizeAfterStop).toEqual((await Deno.stat(testFileName)).size);
+
+	await cleanupTestFile(testFile, testFileName);
+});
+
+Deno.test("renderNextFrame() advances the spinner", async () => {
+	const [testFile, testFileName] = await setupTestFile();
+
+	const kia = await new Kia({
+		text: "",
+		resource: testFile.rid,
+	}).start();
+	await kia?.stopAndPersist();
+
+	const sizeAfterStop = (await Deno.stat(testFileName)).size;
+	await kia?.renderNextFrame();
+
+	// Check that the frame is advancing
+	const sizeAfterNextStop = (await Deno.stat(testFileName)).size;
+	expect(sizeAfterStop).toBeLessThan(sizeAfterNextStop);
+
+	// Check that each frame is only advancing once
+	await kia?.renderNextFrame();
+	expect(sizeAfterNextStop - sizeAfterStop).toEqual(
+		(await Deno.stat(testFileName)).size - sizeAfterNextStop
+	);
+
+	await cleanupTestFile(testFile, testFileName);
+});
+
 Deno.test(
-	"Check renderNextFrame can't be called if spinner is running",
+	"check renderNextFrame can't be called if spinner is running",
 	async () => {
 		const kia = await new Kia("").start();
 		await assertThrowsAsync(async () => {
