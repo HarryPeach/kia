73b1729282b2e1362ed4ab51678cff09a31897a1
GitHub <noreply@github.com>
2022-03-20 01:21:53 +0000

    :arrow_up: Update Dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as Colors from "https://deno.land/std@0.129.0/fmt/colors.ts";
-export * from "https://deno.land/std@0.129.0/testing/asserts.ts";
+export * as Colors from "https://deno.land/std@0.130.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.130.0/testing/asserts.ts";
 export * from "https://deno.land/x/expect@v0.2.9/mod.ts";
-export * from "https://deno.land/std@0.129.0/streams/conversion.ts";
+export * from "https://deno.land/std@0.130.0/streams/conversion.ts";
